1b4d59f2df900c52587c292ef684c33ff998fdd3
IseTrade <IseTrade@gmail.com>
2018-10-24 13:31:26 -0500

    Created controller, and 4 views

---
+++ Superhero/Models/Super Heroes.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Superhero.Models
+{
+    public class Super_Heroes
+    {
+        [Key]
+        public int ID { get; set; }
+        public string HeroName { get; set; }
+        public string AlterEgo { get; set; }
+        public string PrimaryAbilty { get; set; }
+        public string SecondaryAbility { get; set; }
+        public string CatchPhrase { get; set; }
+    }
+}
